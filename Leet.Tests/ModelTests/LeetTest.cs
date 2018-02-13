85500b608df4cd4cbe46ed867ee040a2d17f4e53
Ivan Ramos and jasun feddema <jasun@>
2018-02-12 16:44:57 -0800

    successfully test leetSpeak translation on e but will not pass when the actual program is involved.

--- Leet.Tests/ModelTests/LeetTest.cs
+++ Leet.Tests/ModelTests/LeetTest.cs
@@ -1,5 +1,8 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
 using Leet;
+using Leet.Models;
 
 namespace Leet.Tests
 {
@@ -9,21 +12,8 @@ namespace Leet.Tests
     [TestMethod]
     public void ChangeE_Returns3_String()
     {
-      char[] testMessage = "hello".ToCharArray();
-      char[] leetMessage;
-      foreach (char in testMessage)
-      {
-        if (char == "e" || char =="E")
-        {
-          leetMessage.push("3");
-        }
-        else
-        {
-          leetMessage.push(char);
-        }
-      }
-      string result = string.Join("", leetMessage);
-      return result;
+      Message newMessage = new Message();
+      Assert.AreEqual(true, "h3llo" == newMessage.IntoLeet("hello"));
     }
   }
 }
